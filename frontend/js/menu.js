9f9e5564b40e73606305ffbeda01b310dfb35b2c
san-pok <santoshs.pokhrel@gmail.com>
2024-03-10 20:50:25 +1100

    Updating the menu page

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -1,3 +1,19 @@
+// const themeToggler =  document.querySelector(".theme-toggler");
+
+// themeToggler.addEventListener('click', function (){
+//     document.body.classList.toggle('dark-theme-variables')
+//     themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
+//     themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
+// });
+
+const themeToggler = document.querySelector(".theme-toggler");
+themeToggler.addEventListener("click", ()=>{
+    document.body.classList.toggle('dark-theme-variables')
+  themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
+  themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
+})
+
+
 document.getElementById('signOut').addEventListener('click', function() {
     // Clear authentication tokens or session information
     localStorage.removeItem('userToken'); // Adjust according to how you're storing tokens
@@ -5,3 +21,5 @@ document.getElementById('signOut').addEventListener('click', function() {
     // Redirect to index.html
     window.location.href = 'index.html';
 });
+
+
