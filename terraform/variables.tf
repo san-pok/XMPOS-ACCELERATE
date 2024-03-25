762f7c7ecd113d25ceb3b8572ef30d190d12e9b2
san-pok <santoshs.pokhrel@gmail.com>
2024-03-25 14:31:20 +1100

    check

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "Xmopsteam2"
+}
