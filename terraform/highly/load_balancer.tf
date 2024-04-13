888bdcf0584e5fcfa3df03a08e5f4fb0bf291b84
Am-iir <maharjanamir.101@gmail.com>
2024-04-13 11:36:59 +1000

    Run load balancer with wordpress sg

--- terraform/highly/load_balancer.tf
+++ terraform/highly/load_balancer.tf
@@ -1,35 +1,8 @@
-resource "aws_security_group" "wordpress_lb_sg" {
-  name        = "${var.namespace}-hWordpress_lb_sg"
-  description = "Security group for the WordPress load balancer"
-  vpc_id      = aws_vpc.xmop_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb" "wordpress_lb" {
   name               = "${var.namespace}-hwordpress-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.wordpress_lb_sg.id]
+  security_groups    = [aws_security_group.xmop_wordpress_sg.id]
   subnets            = [
     aws_subnet.xmop_subnet_1.id,
     aws_subnet.xmop_subnet_2.id
