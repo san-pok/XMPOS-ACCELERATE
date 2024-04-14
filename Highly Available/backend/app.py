7a09df330fbccc7388dd9035877a368524a21116
san-pok <santoshs.pokhrel@gmail.com>
2024-04-14 14:16:06 +1000

    Add hghly available to main branch

---
+++ Highly Available/backend/app.py
@@ -0,0 +1,339 @@
+from flask import Flask, jsonify, request, redirect
+from flask_cors import CORS
+import subprocess
+import os
+import boto3
+import json
+from datetime import datetime
+
+highbucket_name = 'amirxmopbucket'
+region_name = 'ap-southeast-2'
+create_bucket_config = {
+    'LocationConstraint': region_name
+}
+
+# Define the prefix
+high_prefix = '/highly'
+
+
+app = Flask(__name__)
+CORS(app, origins='*')
+
+
+@app.route(f'{high_prefix}/deploy', methods=['POST'])
+def deploy_infrastructure():
+    try:
+        # Check if the deploy.sh script exists
+        script_path = '../terraform/deploy.sh'
+        if not os.path.exists(script_path):
+            error_message = 'Error: deploy.sh script not found'
+            return jsonify({'error': error_message}), 500
+
+        # Execute the deploy.sh script
+        subprocess.run([script_path], check=True)
+
+        # Get the load balancer DNS name from Terraform output
+        terraform_output = subprocess.run(['terraform', 'output', 'alb_dns_name'], capture_output=True, text=True)
+        dns_name = terraform_output.stdout.strip()
+
+        # Create JSON data with DNS name and timestamp
+        data = {
+            'load_balancer_dns': dns_name,
+            'timestamp': datetime.now().isoformat()
+        }
+        json_data = json.dumps(data)
+
+        # Create S3 bucket if it doesn't exist
+        s3_client = boto3.client('s3', region_name=region_name)
+        try:
+            s3_client.create_bucket(Bucket=highbucket_name, CreateBucketConfiguration=create_bucket_config)
+        except s3_client.exceptions.BucketAlreadyOwnedByYou:
+            pass  # Bucket already exists and owned by you
+
+        # Upload JSON data to S3 bucket
+        object_key = 'deployment_info.json'
+        s3_client.put_object(Bucket=highbucket_name, Key=object_key, Body=json_data)
+
+        return jsonify({'message': 'Infrastructure deployment triggered successfully by py'}), 200
+    except subprocess.CalledProcessError as e:
+        error_message = f'Error deploying infrastructure by py: {e.stderr.decode()}'
+        return jsonify({'error': error_message}), 500
+    except Exception as e:
+        error_message = f'Unknown error: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
+
+@app.route(f'{high_prefix}/destroy', methods=['POST'])
+def destroy_infrastructure():
+    try:
+        # Check if the destroy.sh script exists
+        script_path = '../terraform/destroy.sh'
+        if not os.path.exists(script_path):
+            error_message = 'Error: destroy.sh script not found'
+            return jsonify({'error': error_message}), 500
+
+        # Execute the destroy.sh script
+        subprocess.run([script_path], check=True)
+
+        return jsonify({'message': 'Infrastructure destruction triggered successfully by py'}), 200
+    except subprocess.CalledProcessError as e:
+        error_message = f'Error destroying infrastructure by py: {e.stderr.decode()}'
+        return jsonify({'error': error_message}), 500
+    except Exception as e:
+        error_message = f'Unknown error: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
+
+@app.route(f'{high_prefix}/validate_form', methods=['POST'])
+def validate_form():
+    try:
+        data = request.get_json()  # Get JSON data from request
+
+        # Perform form validations
+        errors = {}
+        for key, value in data.items():
+            # Check for required fields
+            if not value.strip():
+                errors[key] = 'This field is required'
+            # Check for numeric fields
+            if key.endswith('min_instances') or key.endswith('max_instances'):
+                if not value.isdigit():
+                    errors[key] = 'Please enter a numeric value'
+
+        if errors:
+            return jsonify({'error': 'Form validation failed', 'errors': errors}), 400
+        else:
+
+            # Check if the user chose to create a new key pair
+            if 'new_key_pair_name' in data:
+
+                create_key_pair(data)
+
+                data['key_pair_name'] = data['new_key_pair_name']
+                data.pop('new_key_pair_name')
+
+            else:
+
+                data['key_pair_name'] = data['existing_key_pair_name']
+                data.pop('existing_key_pair_name')
+
+            # Read existing terraform.auto.tfvars content
+            with open('terraform.auto.tfvars', 'r') as f:
+                existing_content = f.readlines()
+
+            # Update validated input fields
+            for key, value in data.items():
+                # Exclude double quotations for numeric fields
+                if key.endswith('min_instances') or key.endswith('max_instances'):
+                    new_line = f"{key} = {value}\n"
+                else:
+                    new_line = f"{key} = \"{value}\"\n"
+
+                # Check if the variable already exists in terraform.auto.tfvars
+                for i, line in enumerate(existing_content):
+                    if key in line:
+                        existing_content[i] = new_line
+                        break
+                else:
+                    # If the variable doesn't exist, append it to the content
+                    existing_content.append(new_line)
+
+            # Write updated content back to terraform.auto.tfvars
+            with open('terraform.auto.tfvars', 'w') as f:
+                f.writelines(existing_content)
+
+                # Trigger infrastructure deployment
+                # response = deploy_infrastructure()
+
+                # if response[1] == 200:  # Check the status code
+                return jsonify({'message': 'Form validation successful and infrastructure deployment triggered'}), 200
+            # else:
+            #   return jsonify({'error': 'Failed to trigger infrastructure deployment after form validation'}), 500
+
+    except Exception as e:
+        error_message = f'Error validating form data: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
+
+@app.route(f'{high_prefix}/existing_key_pairs', methods=['POST'])
+def get_key_pairs():
+    region = request.form.get('region')
+
+    try:
+        ec2 = boto3.client('ec2', region_name=region)
+
+        # Retrieve the list of existing key pairs
+        response = ec2.describe_key_pairs()
+        key_pairs = [key_pair['KeyName'] for key_pair in response['KeyPairs']]
+
+        return jsonify({'existing_key_pairs': key_pairs})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route(f'{high_prefix}/create_key_pair', methods=['POST'])
+def create_key_pair():
+    try:
+
+        region = request.form.get('region')
+        new_key_pair_name = request.form.get('new_key_pair_name')
+
+        ec2 = boto3.client('ec2', region_name=region)
+
+        # Generate a new key pair
+        response = ec2.create_key_pair(KeyName=new_key_pair_name)
+
+        # Save the private key to a file
+        with open(f"{new_key_pair_name}.pem", 'w') as key_file:
+            key_file.write(response['KeyMaterial'])
+
+        return jsonify({'message': 'Key pair created successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route(f'{high_prefix}/get_regions', methods=['GET'])
+def get_all_regions():
+    ec2_client = boto3.client('ec2')
+
+    try:
+        response = ec2_client.describe_regions()
+
+        # Extract region names from the response
+        all_regions = [region['RegionName'] for region in response['Regions']]
+        return all_regions
+    except Exception as e:
+        print(f"Error retrieving regions: {e}")
+        return []
+
+
+@app.route(f'{high_prefix}/amis', methods=['POST'])
+def get_amis_by_os():
+    region = request.form.get('region')
+    os_type = request.form.get('os_type')
+
+    ec2_client = boto3.client('ec2', region_name=region)
+
+    if os_type == 'linux':
+        name_filter = 'amzn2-ami-hvm-*'
+    elif os_type == 'windows':
+        name_filter = 'Windows_Server-*'
+    elif os_type == 'ubuntu':
+        name_filter = 'ubuntu/images/*'
+    else:
+        return jsonify({'error': 'Invalid OS type'}), 400
+
+    filters = [
+        {'Name': 'name', 'Values': [name_filter]},
+        {'Name': 'architecture', 'Values': ['x86_64']},
+        {'Name': 'root-device-type', 'Values': ['ebs']},
+        {'Name': 'virtualization-type', 'Values': ['hvm']},
+        {'Name': 'state', 'Values': ['available']},
+
+
+
+    ]
+
+    # Retrieve AMIs based on the filters
+    try:
+        response = ec2_client.describe_images(Filters=filters)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    # Extract relevant information from the response
+    ami_list = []
+    for image in response['Images']:
+        ami_list.append({
+            'ImageId': image['ImageId'],
+            'Name': image['Name'],
+            'Description': image['Description'],
+        })
+
+    return jsonify(ami_list)
+
+
+@app.route(f'{high_prefix}/instance_types', methods=['POST'])
+def get_instance_types():
+    try:
+        region = request.form.get('region')
+
+        # Initialize AWS EC2 client
+        ec2_client = boto3.client('ec2', region_name=region)
+
+        # Get all instance types available in the region
+        response = ec2_client.describe_instance_type_offerings(
+            LocationType='region',
+            Filters=[
+                {'Name': 'location', 'Values': [region]}
+            ]
+        )
+
+        instance_types = [instance['InstanceType'] for instance in response['InstanceTypeOfferings']]
+
+        return jsonify(instance_types)
+
+    except Exception as e:
+        error_message = f"Error: {e}"
+        return jsonify({"error": error_message})
+
+
+@app.route(f'{high_prefix}/db_engine_types', methods=['POST'])
+def list_db_engine_types():
+    region = request.form.get('region')
+
+    rds_client = boto3.client('rds',
+                              region_name=region)
+    response = rds_client.describe_db_engine_versions()
+
+    engine_types = set()  # Using a set to store unique engine types
+
+    for engine in response['DBEngineVersions']:
+        engine_types.add(engine['Engine'])
+
+    return jsonify(list(engine_types))
+
+
+@app.route(f'{high_prefix}/db_engine_versions', methods=['POST'])
+def list_db_engine_versions():
+    region = request.form.get('region')
+    engine = request.form.get('engine')
+
+    rds_client = boto3.client('rds',
+                              region_name=region)
+
+    response = rds_client.describe_db_engine_versions(Engine=engine)
+
+    mysql_versions = [version['EngineVersion'] for version in response['DBEngineVersions']]
+
+    return jsonify(mysql_versions)
+
+
+@app.route(f'{high_prefix}/deployment_info', methods=['GET'])
+def deployment_info():
+    try:
+
+        s3_client = boto3.client('s3', region_name=region_name)
+
+        # List objects in the specified bucket
+        response = s3_client.list_objects_v2(Bucket=highbucket_name)
+
+        # Extract object keys from the response
+        object_keys = []
+        if 'Contents' in response:
+            object_keys = [obj['Key'] for obj in response['Contents']]
+
+        # Retrieve and extract content of each object
+        object_contents = []
+        for key in object_keys:
+            obj_response = s3_client.get_object(Bucket=highbucket_name, Key=key)
+            obj_content = obj_response['Body'].read().decode('utf-8')
+            object_contents.append(json.loads(obj_content))
+
+        return jsonify({'objects': object_contents}), 200
+    except Exception as e:
+        error_message = f'Error listing S3 objects: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
